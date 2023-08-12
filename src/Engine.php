6943a6a2d6c3e30c4468ac2c426bf35ca32c0c84
Yury Panyukov <Skaner-cj@mail.ru>
2023-08-12 17:40:15 +0700

    исправление синтаксиса файлов

--- src/Engine.php
+++ src/Engine.php
@@ -15,30 +15,28 @@ function evengame()
     line('Answer "yes" if the number is even, otherwise answer "no".');
 
 
-    for ($i = 1; $i < 4; $i+=1) {
+    for ($i = 1; $i < 4; $i += 1) {
+        $check = 'no';
+        $var = mt_rand();
+        if ($var % 2 == 0) {
+            $check = 'yes';
+        }
 
-	$check = 'no';
-	$var = mt_rand();
-	if($var % 2 == 0) {
-	    $check = 'yes';
-	}
-
-	line("Question: {$var}");
+        line("Question: {$var}");
         $answer = prompt('Your answer');
 
-	if($answer === $check){
+        if ($answer === $check) {
 	    line("Correct!");
-	    if($i === 3) {
-		line("Congratulations, {$name}!");
-		break;
-	    }
-	    continue;
-	}
-
-	else {
-	    line("'{$answer}' is wrong answer ;(. Correct answer was '{$check}'.\nLet's try again, {$name}!");
-	    break;
-	}
+            if ($i === 3) {
+                line("Congratulations, {$name}!");
+                break;
+            }
+            continue;
+        }
+        else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$check}'.\nLet's try again, {$name}!");
+            break;
+        }
     }
 }
 
@@ -50,40 +48,38 @@ function calcgame()
     $name = Greeting\greeting();
     line('What is the result of the expression?');
 
-    for ($i = 1; $i < 4; $i+=1){
-
+    for ($i = 1; $i < 4; $i += 1) {
         $first_number = mt_rand(0, 1000);
-	$second_number = mt_rand(0, 1000);
-	$arr = array('-', '+', '*');
-	$curr_op_sw = mt_rand(0, 2);
-	$curr_op = $arr[$curr_op_sw];
-	$show = ("$first_number $curr_op $second_number");
+        $second_number = mt_rand(0, 1000);
+        $arr = array('-', '+', '*');
+        $curr_op_sw = mt_rand(0, 2);
+        $curr_op = $arr[$curr_op_sw];
+        $show = ("$first_number $curr_op $second_number");
 
 
-	switch ($curr_op) {
-	    case '+':
-		$check = $first_number + $second_number;
-	    	break;
-	    case '-':
+        switch ($curr_op) {
+            case '+':
+                $check = $first_number + $second_number;
+            break;
+            case '-':
                 $check = $first_number - $second_number;
-                break;
-	    case '*':
+            break;
+            case '*':
                 $check = $first_number * $second_number;
-                break;
-	}
+            break;
+        }
 
-	line("Question: {$show}");
-	$answer = prompt('Your answer');
+        line("Question: {$show}");
+        $answer = prompt('Your answer');
 
-	if((int)$answer === $check){
-	    line("Correct!");
-            if($i === 3) {
+        if ((int)$answer === $check) {
+            line("Correct!");
+            if ($i === 3) {
                 line("Congratulations, {$name}!");
                 break;
             }
             continue;
         }
-
         else {
             line("'{$answer}' is wrong answer ;(. Correct answer was '{$check}'.\nLet's try again, {$name}!");
             break;
@@ -93,44 +89,41 @@ function calcgame()
 
 
 
-function gcdgame() 
+function gcdgame()
 {
 
     $name = Greeting\greeting();
     line('Find the greatest common divisor of given numbers.');
 
-    for ($i = 1; $i < 4; $i+=1){
-
+    for ($i = 1; $i < 4; $i += 1) {
         $first_number = mt_rand(1, 100);
         $second_number = mt_rand(1, 100);
-	$check = 1;
-	$show = ("$first_number $second_number");
-	$lowest = $first_number;
+        $check = 1;
+        $show = ("$first_number $second_number");
+        $lowest = $first_number;
 
-	if($second_number < $first_number){
-	    $lowest = $second_number;
-	}
+        if ($second_number < $first_number) {
+            $lowest = $second_number;
+        }
 
-	for ($iter = 1; $iter <= $lowest; $iter+=1){
+	for ($iter = 1; $iter <= $lowest; $iter += 1) {
+            if(($first_number % $iter == 0) && ($second_number % $iter == 0)){
+                $check = $iter;
+            }
+        }
 
-	    if(($first_number % $iter == 0) && ($second_number % $iter == 0)){
-		$check = $iter;
-	    }
-	}
 
-
-	line("Question: {$show}");
+        line("Question: {$show}");
         $answer = prompt('Your answer');
 
-        if($answer == $check){
+        if ($answer == $check) {
             line("Correct!");
-            if($i === 3) {
+            if ($i === 3) {
                 line("Congratulations, {$name}!");
                 break;
             }
             continue;
         }
-
         else {
             line("'{$answer}' is wrong answer ;(. Correct answer was '{$check}'.\nLet's try again, {$name}!");
             break;
@@ -146,37 +139,35 @@ function progressiongame()
     $name = Greeting\greeting();
     line('What number is missing in the progression?');
 
-    for ($i = 1; $i < 4; $i+=1){
-
-	$first_number = mt_rand(1, 100);
+    for ($i = 1; $i < 4; $i += 1) {
+        $first_number = mt_rand(1, 100);
         $pitch = mt_rand(1, 30);
-	$show = "{$first_number} ";
-	$check = 0;
-	$blank_space = mt_rand(0, 9);
-	$current = $first_number;
+        $show = "{$first_number} ";
+        $check = 0;
+        $blank_space = mt_rand(0, 9);
+        $current = $first_number;
 
-	if($blank_space == 0){
-	    $show = ".. ";
-	    $check = $first_number;
+        if ($blank_space == 0) {
+            $show = ".. ";
+            $check = $first_number;
+        }
+
+        for ($iter = 1; $iter < 10; $iter += 1) {
+            $current = $current + $pitch;
+
+            if($iter == $blank_space){
+                $show = $show . ".. ";
+                $check = $current;
+            }
+            else {
+                $show = $show . "{$current} ";
+            }
 	}
 
-	for ($iter = 1; $iter <10; $iter += 1){
-
-	    $current = $current + $pitch;
-
-	    if($iter == $blank_space){
-		$show = $show . ".. ";
-		$check = $current;
-	    }
-	    else{
-		$show = $show . "{$current} ";
-	    }
-	}
-
-	line("Question: {$show}");
+        line("Question: {$show}");
         $answer = prompt('Your answer');
 
-        if($answer == $check){
+        if ($answer == $check) {
             line("Correct!");
             if($i === 3) {
                 line("Congratulations, {$name}!");
@@ -199,42 +190,40 @@ function primegame()
     $name = Greeting\greeting();
     line('Answer "yes" if given number is prime. Otherwise answer "no".');
 
-    for ($i = 1; $i < 4; $i+=1){
+    for ($i = 1; $i < 4; $i += 1) {
+        $random_number = mt_rand(2, 100);
+        $show = $random_number;
+        $check = 'yes';
 
-	$random_number = mt_rand(2, 100);
-	$show = $random_number;
-	$check = 'yes';
-
-	if($random_number == 2){
-	    line("Question: {$show}");
+        if ($random_number == 2) {
+            line("Question: {$show}");
             $answer = prompt('Your answer');
 
-            if($answer == $check){
+            if ($answer == $check) {
                 line("Correct!");
-                if($i === 3) {
+                if ($i === 3) {
                     line("Congratulations, {$name}!");
                     break;
                 }
                 continue;
             }
-
             else {
                 line("'{$answer}' is wrong answer ;(. Correct answer was '{$check}'.\nLet's try again, {$name}!");
                 break;
             }
-	}
+        }
 
-	for ($iter = 2; $iter < $random_number; $iter+=1){
-	    if($random_number % $iter == 0){
-	        $check = 'no';
-		break;
-	    }
-	}
+        for ($iter = 2; $iter < $random_number; $iter += 1) {
+            if ($random_number % $iter == 0) {
+            $check = 'no';
+                break;
+            }
+        }
 
         line("Question: {$show}");
         $answer = prompt('Your answer');
 
-        if($answer == $check){
+        if ($answer == $check) {
             line("Correct!");
             if($i === 3) {
                 line("Congratulations, {$name}!");
@@ -242,7 +231,6 @@ function primegame()
             }
             continue;
         }
-
         else {
             line("'{$answer}' is wrong answer ;(. Correct answer was '{$check}'.\nLet's try again, {$name}!");
             break;
--- src/Greeting.php
+++ src/Greeting.php
@@ -13,4 +13,3 @@ function greeting()
 
     return $name;
 }
-
